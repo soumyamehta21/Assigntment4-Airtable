3fe032b09e3c368c8144904fa42bcd2dec393124
Mtech <soumya.mehta@marutitech.com>
2022-02-04 18:03:59 +0530

    Updated version

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ async function fetching(){
     try{
         const url = await fetch("https://api.airtable.com/v0/appyEg2T3hHOc2gAN/Table%201?maxRecords=3&view=Grid%20view", {headers: {"Authorization": "Bearer keynhmixDus3wfxoL"}});
         const data = await url.json();
-        console.log(data);
+        // console.log(data);
+        data.records.forEach((item) => { 
+            // console.log(item.fields.Address);
+            const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            let cnt = 0;
+            td.innerText = 
+        })
     }catch(e){
         console.log(e);
     }
