4e0727dc0a9a643171f4d27902a16cebd800d9ec
Mtech <soumya.mehta@marutitech.com>
2022-02-04 18:13:39 +0530

    Updates

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ async function fetching(){
         data.records.forEach((item) => { 
             // console.log(item.fields.Address);
             const tr = document.createElement('tr');
-            const td = document.createElement('td');
-            let cnt = 0;
-            td.innerText = 
+            const td1 = document.createElement('td');
+            let cnt = 1;
+            td1.innerText = cnt;
+            tr.appendChild(td1);
+            cnt +=  1;
+            const td2 = document.createElement('td');
+            td2.innerText = item.fields.Name;
+            tr.appendChild(td2);
+            const td3 = document.createElement('td');
+            td3.innerText = item.fields.City;
+            tr.appendChild(td3);
+            const td4 = document.createElement('td');
+            td4.innerText = item.fields.Country;
+            tr.appendChild(td4);
+
+            const td2 = document.createElement('td');
+
+
+            table.appendChild(tr);
         })
     }catch(e){
         console.log(e);
