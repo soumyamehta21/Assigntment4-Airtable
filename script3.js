06905db6c13081eb6e1dc3dc2ba0b5fede5fb5b6
Mtech <soumya.mehta@marutitech.com>
2022-02-10 09:52:46 +0530

    Modified Version

--- script3.js
+++ script3.js
@@ -7,7 +7,7 @@ function check(e){
     e.preventDefault();
     const email = document.getElementById('email3');
     const password = document.getElementById('password3');
-    let cnt = 0;
+    let match = 0, eml = 0, pass = 0;
 
     let session = [];
     if(localStorage.getItem('session') === null){
@@ -19,16 +19,27 @@ function check(e){
 
     session.forEach(function(item) {
         if(item.em === email.value && item.ps === password.value){
-            location.href = 'http://127.0.0.1:5502/dashboard.html';
             item.loggedin = 1;
-            cnt = 1;
+            match = 1, eml = 1, pass = 1;
+        }
+
+        if(item.em === email.value){
+            eml = 1;
         }
     });
 
     localStorage.setItem("session", JSON.stringify(session));
 
-    if(cnt == 0){
-        alert("Email Id & Password does not match");
+    if(eml === 1 && match === 0){
+        alert("Email Id & Password does not match!");
         password.value = "";
+    }
+    else if(eml == 0 && match == 0){
+        alert("Your Account Does Not Exist!");
+        email.value = "";
+        password.value = "";
+    }
+    else if(eml === 1 && pass === 1 && match === 1){
+        location.href = 'http://127.0.0.1:5502/dashboard.html';
     }
 }
